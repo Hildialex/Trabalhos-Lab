0c7d09d4b958e1eb632a6e6002a1a597516ff623
Hildialex <hildialex@hotmail.com>
2015-03-26 22:25:15 -0300

    Commit Subtrai

--- calc.h
+++ calc.h
@@ -2,6 +2,6 @@ class Calculadora{
 	public:
 		Calculadora();
 		int soma(int a , int b);
-		int subtrai(int a, int b);
-		int multiplica(int a, int b);
+		int subtrai  (int a, int b);
+		int multiplica  (int a, int b);
 };
